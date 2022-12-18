937855bb33715c4411354175842ce23ebc3bef2a
vsf.viktor <vsf.viktor@gmail.com>
2022-12-18 11:09:19 +0300

    homework complete

---
+++ task2/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+int InputNum (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+int [,] SetArray2DInt (int line, int column)
+{
+    int [,] numArr2D = new int [line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            numArr2D [i, j] = new Random ().Next (0, 100);  
+        }
+    }
+    return numArr2D;
+}
+
+void PrintArray2DInt (int [,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength (0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength (1); j++)
+        {
+            Console.Write (array2D [i, j] + " ");
+        }
+        Console.WriteLine ();
+    }
+}
+
+int [] FindInx (int number)
+{
+    int [] idxArr = new int [2];
+    idxArr [0] = number / 10;
+    idxArr [1] = number % 10;
+    return idxArr;
+}
+
+void PrintUnitArr (int [,] array2D, int [] array)
+{
+    if (array [0] > array2D.GetLength (0) || array [1] > array2D.GetLength (1))
+    {
+        Console.WriteLine ($"There is no such element in the array, line {array [0]} column {array [1]}");
+    }
+    else
+    {
+        Console.WriteLine ($"Ваш элемент: {array2D [array[0] - 1, array [1] - 1]}");
+    }
+}
+
+int m = InputNum ("Set the number of rows: ");
+int n = InputNum ("Set the number of columns: ");
+
+int [,] array = SetArray2DInt (m, n);
+
+Console.WriteLine ($"Your array {m}x{n}:");
+PrintArray2DInt (array);
+
+int num = InputNum ("Enter a two-digit cell index: ");
+int [] index = FindInx (num);
+
+PrintUnitArr (array, index);
---
+++ task3/Program.cs
@@ -0,0 +1,74 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+int InputNum (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+int [,] SetArray2DInt (int line, int column)
+{
+    int [,] numArr2D = new int [line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            numArr2D [i, j] = new Random ().Next (0, 10); // заполняем массив 
+        }
+    }
+    return numArr2D;
+}
+
+void PrintArray2DInt (int [,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength (0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength (1); j++)
+        {
+            Console.Write (array2D [i, j] + " ");
+        }
+        Console.WriteLine ();
+    }
+}
+
+double [] FindAvrArr (int [,] array2D)
+{
+    double [] avrArr = new double [array2D.GetLength (1)];
+    for (int i = 0; i < array2D.GetLength (1); i++)
+    {
+        for (int j = 0; j < array2D.GetLength (0); j++)
+        {
+            avrArr [i] += array2D [j, i];
+        }
+        avrArr [i] = avrArr [i] / array2D.GetLength (0);
+    }
+    return avrArr;
+};
+
+void PrintArray (double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = Math.Round (array [i], 1);
+        Console.Write (array [i] + " ");
+    }
+        Console.WriteLine ();
+}
+
+int m = InputNum ("Enter the number of lines: ");
+int n = InputNum ("Enter number of columns: ");
+
+int [,] array2D = SetArray2DInt (m, n);
+
+PrintArray2DInt (array2D);
+
+double [] arrayAvr = FindAvrArr (array2D);
+
+Console.WriteLine ($"Arithmetic mean of column elements:");
+PrintArray (arrayAvr);
